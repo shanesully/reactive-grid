c6fc88b8186f44b7b46558f2a7572aca96c776e0
sully <shanesully01@gmail.com>
2019-04-09 13:48:11 +0100

    Basic Spring Shell Test
    Created a dummy test to verify Spring Shell testable

--- src/test/java/com/tweethub/reactivegrid/ReactiveGridApplicationTests.java
+++ src/test/java/com/tweethub/reactivegrid/ReactiveGridApplicationTests.java
@@ -2,10 +2,15 @@ package com.tweethub.reactivegrid;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.shell.Input;
+import org.springframework.shell.Shell;
 import org.springframework.shell.jline.InteractiveShellApplicationRunner;
 import org.springframework.shell.jline.ScriptShellApplicationRunner;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(properties = {
@@ -14,8 +19,23 @@ import org.springframework.test.context.junit4.SpringRunner;
 })
 public class ReactiveGridApplicationTests {
 
+	@Autowired
+	private Shell shell;
+
+	@Test
+	public void addTwoNumbers(){
+		Object result = shell.evaluate(new Input(){
+			@Override
+			public String rawText() {
+				return "add 1 1";
+			}
+
+		});
+
+		assertThat(result).isEqualTo(2);
+	}
+
 	@Test
 	public void contextLoads() {
 	}
-
 }
